82b88f2ebd46f290fdc2a768e0d2659912be78ce
UTKARSH SINGH VIVEK <96197640+utkarsh462255@users.noreply.github.com>
2023-03-07 10:44:50 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,12 @@
+var x=document.getElementById("hands");
+var y=document.getElementById("animcon");
+function closeye()
+{
+	y.style.backgroundImage="url(https://raw.githubusercontent.com/naaficodes/Monkey-Login/master/images/monkey_pwd.gif)";
+	x.style.marginTop="0%";
+}
+function openeye()
+{
+	y.style.backgroundImage="url(https://raw.githubusercontent.com/naaficodes/Monkey-Login/master/images/monkey.gif)";
+	x.style.marginTop="110%";
+}
